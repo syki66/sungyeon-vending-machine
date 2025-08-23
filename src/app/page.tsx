0d813c4e1255dc4860f92b8162620b11e41ca5eb
syki66 <66syki@gmail.com>
2025-08-23 22:42:32 +0900

    feat: 자판기 디스플레이 문구 기능 추가 - 잔액 텍스트 상시 표기 - 잔액과 액션에 따른 메인 문구 추가 - 잔액 음수 방지 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,42 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [balance, setBalance] = useState<number>(0); // 잔액
-  const [displayText, setDisplayText] =
-    useState<string>('현금 또는 카드를 투입해주세요.'); // 디스플레이에 표시되는 문구
-
+  const [displayMainText, setDisplayMainText] =
+    useState<string>('현금 또는 카드를 투입해주세요.'); // 디스플레이에 표시되는 메인 문구
+  const [displaySubText, setDisplaySubText] =
+    useState<string>('현재 잔액: 0원'); // 디스플레이의 오른쪽 하단에 표시되는 잔액 문구
+  // 잔액 변경 함수
   const handleBalanceChange = (amount: number) => {
-    setBalance((prev) => prev + amount);
+    // 디스플레이 메인 텍스트 업데이트
+    if (amount > 0) {
+      // 금액 투입일 경우
+      setDisplayMainText(`${amount.toLocaleString()}원이 투입되었습니다.`);
+    } else if (amount < 0) {
+      // 음료 구매일 경우
+      let usedText = '';
+      if (amount === -PRICE_LIST.cola) {
+        usedText = `콜라를 뽑았습니다.`;
+      } else if (amount === -PRICE_LIST.water) {
+        usedText = `물을 뽑았습니다.`;
+      } else if (amount === -PRICE_LIST.coffee) {
+        usedText = `커피를 뽑았습니다.`;
+      }
+      // 버튼 비활성화가 아닌 경우에만 문구 출력
+      if (balance + amount >= 0 && usedText) {
+        setDisplayMainText(`${usedText}`);
+      } else {
+        // 비활성화된 경우 잔액 부족 문구 출력
+        setDisplayMainText('잔액이 부족합니다.');
+      }
+    }
+
+    // 잔액 업데이트 (잔액이 음수가 되지 않도록 처리)
+    setBalance((prev) => (prev + amount < 0 ? prev : prev + amount));
   };
 
+  // 잔액이 변경될 때마다 디스플레이 서브 텍스트 업데이트
   useEffect(() => {
-    if (balance > 0) {
-      setDisplayText(`${balance.toLocaleString()}원 투입됨`);
-    }
+    setDisplaySubText(`현재 잔액: ${balance.toLocaleString()}원`);
   }, [balance]);
 
   return (
